18c61230e470c7a3ba0fa419b361ffa85f9c7a22
hamid19471 <hamid.19471@gmail.com>
2023-04-05 02:26:29 +0330

    Create: most component files only

---
+++ src/components/HeroSection/HearoSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HearoSection = () => {
+    return <div>HearoSection</div>;
+};
+
+export default HearoSection;
---
+++ src/components/Testimonial/estimonial.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const estimonial = () => {
+    return <div>estimonial</div>;
+};
+
+export default estimonial;
