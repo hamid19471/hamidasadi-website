93c14216c11c5b12fb87a61ed382af902df50375
hamid19471 <hamid.19471@gmail.com>
2023-04-07 16:03:33 +0330

    Update: import main layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Layout from "./layout";
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Layout>hamid</Layout>
+        </div>
+    );
 }
 
 export default App;
