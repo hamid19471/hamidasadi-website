2f7a050e51d99c1ca6c08e8fc7082c62d7e8a81d
hamid19471 <hamid.19471@gmail.com>
2023-04-07 13:51:01 +0330

    Update: add custom theme code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,33 @@ module.exports = withMT({
         "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        color: {
+            "aj-bg": "var(--bg-color)",
+            "aj-text": "var(--text-color)",
+            "aj-primary": "var(--primary-color)",
+            "aj-border": "var(--border-color)",
+        },
+        fontFamily: {
+            sans: ["Oswald", "sans-serif"],
+        },
+        extend: {
+            borderRadius: {
+                "btn-radius": "0.6rem",
+                "box-radius": "0.6rem",
+            },
+            fontSize: {
+                "btn-text": "0.9rem",
+                "small-heading": "1.3rem",
+                "main-heading": "2rem",
+                paragraph: "1rem",
+            },
+            fontWeight: {
+                "btn-text": "600",
+                "small-heading": "400",
+                "main-heading": "800",
+                paragraph: "400",
+            },
+        },
     },
     plugins: [],
 });
