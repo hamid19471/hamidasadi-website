a1e1fa5f8c144189946e22ebcda69df64ffc45ea
hamid19471 <hamid.19471@gmail.com>
2023-04-12 12:40:02 +0330

    Update: footer design

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import { menuItems } from "../../FakeAPI/MenuItem";
+import logo from "../../assets/logo-footer.png";
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="w-full bg-[#47aaf2] py-16">
+            <div className="container flex flex-col items-center justify-center">
+                <div>
+                    <img src={logo} alt="" className="w-[220px]" />
+                </div>
+                <div className="flex items-center justify-center my-10 text-black focus:border-none"></div>
+
+                <div className="flex items-center justify-center gap-5">
+                    {menuItems.map((item) => (
+                        <span
+                            className="text-[14px] font-normal text-black uppercase cursor-pointer hover:text-black hover:scale-125 transition duration-300"
+                            key={item.id}
+                        >
+                            {item.name}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <hr className="container mt-10 border-t-black"></hr>
+            <div className="container mx-auto mt-10 -mb-8 text-center text-black">
+                ALL RIGHT RESERVER Ⓒ 2023
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
