fc9b8d4c93619f3723ef60e1753ea46f999bdfdf
hamid19471 <hamid.19471@gmail.com>
2023-04-21 17:26:47 +0330

    Update: remove unused test code

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider>
-            <BrowserRouter>
                 <App />
-            </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>,
 );
