8c89eb28f3caa469729bfb73f772bc8b79ac8c2a
hamid19471 <hamid.19471@gmail.com>
2023-04-22 20:54:20 +0330

    Update: add more style for button compoonnent

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({ name, path, primary }: Props) => {
     return (
         <>
             <button
-                className={`m-[10px] text-content font-extralight py-2 px-5 rounded-lg duration-300 ${
+                className={` text-content font-extralight py-2 px-5 rounded-lg duration-300 uppercase tracking-wider ${
                     primary
                         ? "bg-primary text-black hover:scale-110"
                         : "border border-primary text-white hover:bg-primary hover:scale-110 hover:text-black"
