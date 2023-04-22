8da9022f2adab614f98ac4bee84b8ca9efab7f52
hamid19471 <hamid.19471@gmail.com>
2023-04-23 01:56:47 +0330

    Create: section title and subtitle component

---
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -0,0 +1,21 @@
+interface Props {
+    title: string;
+    subTitle: string;
+}
+
+const SectionHeading = ({ title, subTitle }: Props) => {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center uppercase my-[120px]">
+                <p className="text-contet font-extralight tracking-wide">
+                    {subTitle}
+                </p>
+                <h3 className="text-heading text-primary font-bold tracking-wider">
+                    {title}
+                </h3>
+            </div>
+        </>
+    );
+};
+
+export default SectionHeading;
