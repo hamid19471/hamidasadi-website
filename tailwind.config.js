b95b809abd10f728d4c73c75f31cf684552ce6e9
hamid19471 <hamid.19471@gmail.com>
2023-04-26 13:59:03 +0330

    Update: main tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-        "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-        "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-    ],
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         colors: {
             primary: "#47aaf2",
@@ -20,4 +13,4 @@ module.exports = withMT({
         },
     },
     plugins: [],
-});
+};
