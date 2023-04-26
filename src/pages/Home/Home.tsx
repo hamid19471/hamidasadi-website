2f4e3257ac00eba6afbfa33c065031da01057038
hamid19471 <hamid.19471@gmail.com>
2023-04-26 12:36:32 +0330

    Update: add experiences and services component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Clients from "../../components/Clients/Clients";
+import Experiences from "../../components/Expreriences/Experiences";
 import HearoSection from "../../components/HeroSection/HearoSection";
+import Services from "../../components/Services/Services";
 import Layout from "../../layout";
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
             <HearoSection />
             <AboutMe />
             <Clients />
+            <Experiences />
+            <Services />
         </Layout>
     );
 };
