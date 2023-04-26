13f8fca683a4ff003387ec96c5acb670de3214ba
hamid19471 <hamid.19471@gmail.com>
2023-04-26 12:35:44 +0330

    Update: single experiences conponent and all expreriences

---
+++ src/components/SingleExperience/SingleExperience.tsx
@@ -0,0 +1,24 @@
+import { BsCheck2Circle } from "react-icons/bs";
+
+interface Props {
+    name: string;
+    description: string;
+}
+
+const SingleExperience = ({ name, description }: Props) => {
+    return (
+        <div className="flex items-start justify-start gap-2 uppercase">
+            <div>
+                <BsCheck2Circle className="text-[20px] font-bold text-primary" />
+            </div>
+            <div>
+                <h3 className="text-[20px] -mt-1">{name}</h3>
+                <p className=" font-extralight tracking-wider text-[15px] text-white/80">
+                    {description}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default SingleExperience;
